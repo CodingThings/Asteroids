041257e65f20802efca98a25e0e2b04386a1e799
David <david.roszkowski@gmail.com>
2025-07-31 00:40:17 -0500

    Added fps to game screen

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pygame
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while(True):
         for event in pygame.event.get():
@@ -10,6 +14,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
